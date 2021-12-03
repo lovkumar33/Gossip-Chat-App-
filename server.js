0f0f5f3910b91628bc831529893e30d74cf1d4b0
lovkumar33 <lovkumar33@gmail.com>
2021-12-03 09:51:19 +0530

    database part

--- server.js
+++ server.js
@@ -1,8 +1,49 @@
 const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
+//
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://localhost:27017/mydb',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+db.on('error',()=>console.log("Error in Connecting to Database"));
+db.once('open',()=>console.log("Connected to Database"))
+
+app.post("/sign_up",(req,res)=>{
+    var name = req.body.name;
+    var email = req.body.email;
+    var phno = req.body.phno;
+
+    var data = {
+        "name": name,
+        "email" : email,
+        "phno": phno
+    }
+
+    db.collection('users').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+
+    return res.redirect('signup_success.html')
+
+})
+//
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
@@ -40,5 +81,5 @@ io.on("connection",(socket)=>{
 /* Socket.io Setup Ends */
 
 server.listen(port,()=>{
-  console.log("Server started at"+port);
+  console.log("Server started at "+port);
 });
